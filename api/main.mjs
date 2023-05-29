27c6e8e1e05f3923d4ce243922b4a6811c79ba94
Filinto Delgado <filintodelgado@gmail.com>
2023-05-29 22:16:08 +0100

    New Custom Event model

--- api/main.mjs
+++ api/main.mjs
@@ -1,5 +1,12 @@
 /**
- * Main cinescape module
+ * The CineScape is a School project to make a educational ScapeRoom.
+ * We made this Framework/API to make the whole process a lot easier.
+ * 
+ * This model includes things like:
+ * 1. Puzzle Components;
+ * 1. Timer;
+ * 1. Event Model.
+ * 
  * @module cinescape
  */
 
@@ -16,6 +23,10 @@ export { Puzzle, PuzzleSelect, PuzzleChoose }
 /* Timer */
 import { Timer } from "./timer.mjs";
 export { Timer };
+
+/* Event Model */
+import { EventModel } from "./eventModel.mjs";
+export { EventModel };
 
 /* Defautl */
 export default { PuzzleSelect, Timer}
