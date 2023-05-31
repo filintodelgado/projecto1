dda7f1b5b9ccdedff4f18125cdcc25f492d3f83f
rocha1202 <40220247@esmad.ipp.pt>
2023-05-31 16:34:30 +0100

    ligacao de login com js

--- js/login/login.js
+++ js/login/login.js
@@ -1,60 +1,25 @@
-let login = true
-
-let username = document.getElementById("username")
-let date = document.getElementById("date")
-let local = document.getElementById("local")
-let sex = document.getElementById("sex")
-
-let loginButton = document.getElementById("login-button")
-
-let Change_Mode = document.getElementById("change-mode")
-Change_Mode.addEventListener("click", function(){
-    ChangeMode()
-})
-
-function ChangeMode(){
-    if(!login){
-        Change_Mode.innerHTML = `<h4>Criar Conta</h4>`
+import { login, home } from "./module.mjs";
 
 
-        let TitleContent = document.getElementById("title-content")
-        if(document.body.classList == "light"){
-            TitleContent.innerHTML = `<img src="../assets/login/login_light.png" height="190" id="ticket-title" style="width: 100%;">`
-        }else{
-            TitleContent.innerHTML = `<img src="../assets/login/login_dark.png" height="190" id="ticket-title" style="width: 100%;">`
+var email = document.getElementById('txtEmail');
+var pass = document.getElementById('txtPass');
+
+
+function loginVerifi() {
+    let btn = document.getElementById('login-button');
+    let btnTexto = btn.textContent;//passa só o texto do botão
+
+    if (btnTexto == 'Entrar') {
+        const success = login(email.value, pass.value, remenber.checked)
+
+        if (!success) {
+            div.className = "error"
+            return
         }
 
-        let TopLogin = document.getElementById("top-login")
-        TopLogin.innerHTML = `<h1>Login</h1>`
+        home()
 
-        username.style.display = "none"
-        date.style.display = "none"
-        local.style.display = "none"
-        sex.style.display = "none"
-
-        loginButton.innerHTML = "<h4>Entrar</h4>"
-
-        login = true
-    }else{
-        Change_Mode.innerHTML = `<h4>Login</h4>`
-
-        let TitleContent = document.getElementById("title-content")
-        if(document.body.classList == "light"){
-            TitleContent.innerHTML = `<img src="../assets/login/signup_light.png" height="190" id="ticket-title" style="width: 100%;">`
-        }else{
-            TitleContent.innerHTML = `<img src="../assets/login/signup_dark.png" height="190" id="ticket-title" style="width: 100%;">`
-        }
-
-        let TopLogin = document.getElementById("top-login")
-        TopLogin.innerHTML = `<h1>Criar Conta</h1>`
-        
-        username.style.display = "flex"
-        date.style.display = "flex"
-        local.style.display = "flex"
-        sex.style.display = "flex"
-
-        loginButton.innerHTML = "<h4>Registar</h4>"
-
-        login = false
+    } else {
+        alert("registo")
     }
 }
---
+++ js/login/module.mjs
@@ -0,0 +1,146 @@
+/** 
+ * Contem routinas e utilitario gericos
+ * @module escapeRoom/module 
+ */
+
+/**
+ * Redireciona o site para o index (/)
+ * 
+ * Só funciona se o protocolo for diferente de file://
+ * @param {number} time - quanto tempo em segundos para fazer o redirecionamento
+ * @returns {boolean}
+ */
+export function home(time=2) {
+  // só faz o reload se estiver a ser executado de um servidor
+  // não funciona quando esta a executar usando o arquivo diretamente
+  if(location.protocol == 'file:') return false
+  
+  // mudar o path faz a pagina fazer reload para a para o novo path
+  setTimeout(() => {
+    location.pathname = "/"
+  }, time * 500)
+}
+
+/**
+ * Define o metodo de armazenamento a se usar
+ * @param {bool} remenber - Define se o armazenamento é permanente ou temporario
+ * @returns {Storage} O tipo de armazenamento a ser usado que pode ser localStorage(permanente)
+ * ou sessionStorage(temporario)
+ */
+function storageType(remenber) {
+  return remenber ? localStorage : sessionStorage;
+}
+
+
+/**
+ * Retorna a palavra-passe do usuario
+ * 
+ * Pode ser usado para validar um usuario
+ * @param {string} username - O nome do usuario
+ * @returns {string|null} A palavrapasse desse usuario ou null caso não exista
+ */
+function getUser(username) {
+  return localStorage.getItem(username)
+}
+
+/**
+ * Caso um usuario esteja logado retorna o usuario logado
+ * @returns {string} o nome do usuario logado
+ */
+export function getLogin() {
+  // retorna o login armazenado priorizando o login temporario
+  return sessionStorage.getItem("login") ? sessionStorage.getItem("login") : localStorage.getItem("login")
+}
+
+/**
+ * Remove o login armazenado
+ * @returns {string|null} O nome de usuario que estava logado ou null
+ * caso não existia um login
+ */
+export function logout() {
+  /** O nome do usuario logado anteriomente */
+  let previousLogin = getLogin()
+
+  // remove o login dos dois tipos de armazenamento
+  sessionStorage.removeItem("login")
+  localStorage.removeItem("login")
+
+  console.log(`Deslogado do usuario ${previousLogin}.`)
+
+  return previousLogin
+}
+
+/**
+ * Faz o login de uma conta já existente
+ * Substitui o login caso já existe
+ * @param {string} username - O nome do usuario
+ * @param {string} password - A palavra-passe do usuario
+ * @param {bool} remenber - Caso seja verdadeiro o login é guardado é guardado permanentimente 
+ * @returns {string|boolean} O username caso o login for feiro com sucesso do contrario falso
+ * caso a conta já existia
+*/
+export function login(username, password, remenber) {
+  /** localStorage para permanente ou sessionStorage para temporario */
+  let storageMethod = storageType(remenber);
+
+  /** A palavra-passe armazenada */
+  let storedPassword = getUser(username);
+
+  /** lista de compras */
+  let cart = localStorage.getItem("cart")
+
+  if (cart[username] == null){
+    cart[username] = [];
+    localStorage.setItem("cart", cart)
+  }
+  
+  // caso o usuario não existe
+  if(storedPassword == null) {
+    window.alert(`O utilizador ${username} não existe.`);
+    console.error(`O usuario ${username} não existe.`);
+    
+    return false;
+  }
+  
+  // a palavra-passe está errado
+  if(storedPassword != password) {
+    window.alert(`A palavra-passe do utilizador ${username} está errada.`);
+    console.error(`Palavra-passe para usuario ${username} está errado.`);
+    return false;
+  }
+
+  // remove o login anterior para evitar colisão
+  logout();
+  
+  // loga o usuario definindo o login
+  storageMethod.setItem("login", username);
+  console.log(`Logado como ${username}.`);
+  window.alert(`Bem-vindo/a ${username}.`);
+  return username;
+}
+
+/**
+ * Cria uma conta e faz o login
+ * @param {string} username - O nome do usuario
+ * @param {string} password - A palavra-passe do usuario
+ * @param {boolean} remenber - Caso seja verdadeiro o login é guardado permanetimente
+ * @returns {username | boolean} o username caso o login for feito com sucesso do contrario falso
+ * caso a conta já existia
+ */
+export function signUp(username, password, remenber) {
+  // guarda a conta na memoria permanente
+  // retona falso caso o usuario ja existe
+  if(getUser(username) != null) {
+    console.error(`O usuario ${username} já existe.`);
+    window.alert(`O utilizador ${username} já existe.`);
+    return false;
+  }
+
+  // guarda os dados do login no local storage (permanente)
+  localStorage.setItem(username, password)
+  window.alert(`Utilizador ${username} criado com sucesso.`);
+  console.log(`Usuario ${username} criado com sucesso.`);
+
+  // faz o login e retorna o nome do usuario
+  return login(username, password, remenber);
+}
