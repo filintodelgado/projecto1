917ea697f86743b8dd3dcdb576e70d680e3a057d
Diogo Lopes <40220401@esmad.ipp.pt>
2023-06-07 11:04:36 +0100

    Criação de novas páginas

---
+++ js/atividades.js
@@ -0,0 +1,7 @@
+let at1 = document.getElementById("at1")
+let at2 = document.getElementById("at2")
+let at3 = document.getElementById("at3")
+let at4 = document.getElementById("at4")
+let at5 = document.getElementById("at5")
+
+at2.classList.toggle("at-done")
