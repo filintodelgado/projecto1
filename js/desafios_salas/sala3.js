c0403a302b7f5f27fb3ceb49a244901558765e7e
rocha1202 <40220247@esmad.ipp.pt>
2023-06-14 14:38:46 +0100

    sala3 resposta-curta

--- js/desafios_salas/sala3.js
+++ js/desafios_salas/sala3.js
@@ -2,3 +2,43 @@ const porta3 = document.getElementById('porta').addEventListener('click', functi
     window.location.href = '../html/escapeRoom.html';
 
 });
+const filme = document.getElementById('verde1fila').addEventListener('click', function () {
+
+    // Seleciona elementos do DOM
+    //var openModalBtn = document.getElementById('openModalBtn');
+    var modal = document.getElementById('modal');
+    var closeBtn = document.getElementsByClassName('close')[0];
+    var iframeContainer = document.getElementById('iframeContainer');
+
+    // URL do iframe
+    var iframeURL = '../html/desafios/nome_filme.html';
+
+    // Função para carregar o iframe
+    function loadIframe() {
+        var iframe = document.createElement('iframe');
+        iframe.src = iframeURL;
+        iframe.width = '100%';
+        iframe.height = '100%';
+        iframe.frameborder = '0';
+        iframeContainer.appendChild(iframe);
+    }
+
+    // Abre o modal quando o botão é clicado e carrega o iframe
+    modal.style.display = 'block';
+    loadIframe();
+
+
+    // Fecha o modal quando o botão de fechar é clicado
+    closeBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+        iframeContainer.innerHTML = ''; // Limpa o contêiner do iframe ao fechar
+    });
+
+    // Fecha o modal quando o usuário clica fora dele
+    window.addEventListener('click', function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+            iframeContainer.innerHTML = ''; // Limpa o contêiner do iframe ao fechar
+        }
+    });
+});
